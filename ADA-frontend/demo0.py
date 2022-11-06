a9058eb2540edf29922e81675b531979c39b18b8
Narges Mehran <39765584+SiNa88@users.noreply.github.com>
2022-11-06 11:06:46 +0000

    Add files via upload

--- ADA-frontend/demo0.py
+++ ADA-frontend/demo0.py
@@ -1,6 +1,5 @@
 from interface_json import PipelineDataContainer
 from flask import Flask, render_template, Response, request, jsonify, make_response, redirect
-from werkzeug.utils import secure_filename
 import os
 import json
 import yaml
@@ -15,6 +14,8 @@ PORT_NUMBER: int = 5000
 # Create a new Flask application
 app = Flask(__name__)
 
+FILE_PATH = '3ApplicationLogic.json'
+
 with open('requirements.yaml', 'r') as file_read:
 	requirement_list = json.dumps(yaml.load(file_read, Loader=yaml.FullLoader))
 
@@ -22,23 +23,22 @@ with open('resources.json', 'r') as file_read:
 	resource_list0 = json.load(file_read)
 resource_list = json.dumps(resource_list0)
 
-'''with open('D:\\00Research\\matching\\scheduler\\ADA-PIPE-Frontend-new\\3ApplicationLogic.json', 'r') as openfile:
+with open(FILE_PATH, 'r') as openfile:
     json_object = json.load(openfile)
-data = json.dumps(json_object)
-#pdc = PipelineDataContainer(FILE_PATH)'''
 
-with open('3ApplicationLogic.json', 'r') as openfile:
+data = json.dumps(json_object)
+#pdc = PipelineDataContainer(FILE_PATH)
+
+with open(FILE_PATH, 'r') as openfile:
     json_object = json.load(openfile)
 data = json.dumps(json_object)
 
 @app.route('/', methods=['GET', 'POST'])
 def get_homepage():
     if request.method == 'POST':
-        if request.form.get('action1') == 'INPUT1 - requirements (template)':
+        if request.form.get('action1') == 'INPUT1 - requirements':
             return redirect("/requirements", code=302) #Response(requirement_list, mimetype='text/yaml')
-        elif request.form.get('action2') == 'INPUT1 - requirements (from BC)':
-            return redirect("/upload", code=302)
-        elif  request.form.get('action3') == 'INPUT2 - resources':
+        elif  request.form.get('action2') == 'INPUT2 - resources':
             return redirect("/resources", code=302) #Response(resource_list, mimetype='application/json')
         else:
             return redirect("/schedules", code=302) #Response(data, mimetype='application/json')
@@ -57,41 +57,28 @@ def index():
         #keycloak_token = keycloak_utils._get_keycloak_token(username,password_candidate)
         #access_token = keycloak_token['access_token']
         #print(access_token)
+
         #user_info = keycloak_utils.verify_access_token(access_token)
         #print("Token is verified :D :D ",user_info)
         #if(user_info == True):
-        if request.method == 'POST':
-            if request.form.get('action1') == 'INPUT1 - requirements (template)':
-                return redirect("/requirements", code=302) #Response(requirement_list, mimetype='text/yaml')
-            elif request.form.get('action2') == 'INPUT1 - requirements (from BC)':
-                return redirect("/upload", code=302)
-            elif request.form.get('action3') == 'INPUT2 - resources':
-                return redirect("/resources", code=302) #Response(resource_list, mimetype='application/json')
-            else:
-                return redirect("/schedules", code=302) #Response(data, mimetype='application/json')
+	        if request.method == 'POST':
+	                if request.form.get('action1') == 'INPUT1 - requirements':
+	                    return redirect("/requirements", code=302)
+	                elif  request.form.get('action2') == 'INPUT2 - resources':
+	                    return redirect("/resources", code=302) #Response(resource_list, mimetype='application/json')
+	                else:
+	                    return redirect("/schedules", code=302) #Response(data, mimetype='application/json')
     elif request.method == 'GET':
         #print(":D")
-        return render_template("./index.html", form=request.form)
+        return render_template("index.html", form=request.form)
     #print(":D:D")
-    return render_template("./index.html")
+    return render_template("index.html")
 
-@app.route('/upload')
-def upload_page():
-   return render_template('./upload.html')
-
-@app.route('/uploader', methods = ['GET', 'POST'])
-def upload_file():
-   if request.method == 'POST':
-      f = request.files['file']
-      filename = secure_filename(f.filename)
-      f.save(os.path.join('./uploaded/', filename))
-      return ('file uploaded successfully' and redirect("/", code=302))
-
-'''@app.route("/features", methods=['GET', 'POST'])
+@app.route("/features", methods=['GET', 'POST'])
 def index0():
     if request.method == "POST":
         return Response(file_file, mimetype='application/html' )
-'''
+
 @app.route("/requirements", methods=['GET'])
 def index1():
     if request.method == 'GET':
@@ -119,4 +106,3 @@ if __name__ == '__main__':
         IP_address = socket.gethostbyname(host_name)
         print(f'Running on http://{IP_address}/{PORT_NUMBER}/ (Press CTRL+C to quit)')
         serve(app, host=HOST_NUMBER, port=PORT_NUMBER)
-
