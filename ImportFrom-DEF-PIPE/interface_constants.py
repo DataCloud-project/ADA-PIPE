0c7ed19732a0498ffe76c9364a2050b9cb9da466
Narges Mehran <39765584+SiNa88@users.noreply.github.com>
2023-01-03 12:11:11 +0100

    Add files via upload

---
+++ ImportFrom-DEF-PIPE/interface_constants.py
@@ -0,0 +1,24 @@
+PIPELINE_NAME: str = 'pipelineName'
+
+
+STEPS_NAME: str = 'name'
+
+STEPS_IMPLEMENTATION: str = 'implementation:'
+STEPS_ENVIRONMENTPARAMETERS: str = 'environmentParameters'
+STEPS_RESOURCEPROVIDER: str = 'resourceProvider'
+STEPS_PREVIOUS: str = 'previous'
+STEPS_EXECUTIONREQUIRMENT: str = 'executionRequirement'
+
+STEPS_DOCKER_IMAGE: str = 'image:'
+STEPS_DOCKER_CREDENTIALS: str = 'dockerCredentialsUsing'
+STEPS_DOCKER_USERNAME: str = 'dockerUsername'
+STEPS_DOCKER_PASSWORD: str = 'dockerPassword'  
+STEPS_DOCKER_CUSTOM_REGISTRY: str = 'dockerCustomRegistry'
+
+STEPS_REQ_MININS: str = 'min-instance'
+STEPS_REQ_MAXINS: str = 'max-instance'
+STEPS_REQ_MINMEM: str = 'min-ram-mb:'
+STEPS_REQ_MINCPU: str = 'min-cpu:'
+STEPS_REQ_MAXCPU: str = 'max-cpu:'
+STEPS_REQ_GPU: str = 'gpu-availability'
+
