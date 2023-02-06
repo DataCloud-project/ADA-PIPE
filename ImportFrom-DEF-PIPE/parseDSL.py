8147b7d7928bc40130abf1e0fe2f5f397b417e18
Narges Mehran <39765584+SiNa88@users.noreply.github.com>
2023-02-06 13:13:28 +0100

    Update parseDSL.py

--- ImportFrom-DEF-PIPE/parseDSL.py
+++ ImportFrom-DEF-PIPE/parseDSL.py
@@ -60,7 +60,7 @@ output=output.replace(output[len(output)-37:], "")
 print(output)
 parseDSL (output)'''
 
-token_call = ["curl", "--location", "--request", "POST", "https://datacloud-auth.euprojects.net/auth/realms/user-authentication/protocol/openid-connect/token", "--header", "Content-Type: application/x-www-form-urlencoded", "--data-urlencode", "username=testuser", "--data-urlencode", "password=cT2!3jB0H43sbl", "--data-urlencode", "realm=user-authentication", "--data-urlencode", "client_id=def_frontend", "--data-urlencode", "grant_type=password"]
+token_call = ["curl", "--location", "--request", "POST", "https://datacloud-auth.euprojects.net/auth/realms/user-authentication/protocol/openid-connect/token", "--header", "Content-Type: application/x-www-form-urlencoded", "--data-urlencode", "username=?????", "--data-urlencode", "password=??????", "--data-urlencode", "realm=user-authentication", "--data-urlencode", "client_id=def_frontend", "--data-urlencode", "grant_type=password"]
 p = subprocess.Popen(token_call, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 output, err = p.communicate()
 dict = json.loads(output.decode())
