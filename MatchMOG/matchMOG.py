20b36471aaf0ad0b81822ba09bb0520766ece0a4
Narges Mehran <39765584+SiNa88@users.noreply.github.com>
2023-02-19 13:43:33 +0100

    Add files via upload

--- MatchMOG/matchMOG.py
+++ MatchMOG/matchMOG.py
@@ -3,15 +3,13 @@ from operator import itemgetter
 from matching.games import HospitalResident
 import yaml
 
-microservices = ["encoding", "splittingstream", "training"]
+microservices = ["detection_RIGHT", "detection_LEFT, broker", "training: [fusion, overlay]"]
 
+encode = [2.69,3.16,2.7,28,61] #seconds
+splittingstream = [11.7,  11.2, 2.1,   2.5,   3.1 ] #seconds
+training = [33.23, 56.746 , 36.253 , 466.830 , 1000] #seconds
 
-
-encode = [2.69,3.16,20.64,28,60] #seconds
-splittingstream = [11.7,  11.2, 3.1,   3.1,   3.1 ] #seconds
-training = [33.23, 56.746 , 232.253 , 466.830 , 1000] #seconds
-
-#              0	 1	2     3     4 
+#              0	 			1					2     			3     				4 
 resources = ["DataCloud-wp1","DataCloud-wp2","DataCloud-edge-0","DataCloud-edge-1","DataCloud-edge-2"]
 
 
@@ -29,7 +27,7 @@ video_size = [2000000, 14000000, 28000000, 60000000, 204800000] #bits
 SIZE = 200
 
 
-BW_r = [60000000 , 920000000, 450000000 , 800000000 , 328000000]#bps
+BW_r = [60000000 , 920000000, 800000000 , 450000000 , 328000000]#bps
 
 
 dictlistResources = list( {} for i in range(len(resources)) )
@@ -44,25 +42,30 @@ T[0][0] = encode[0] + ((video_size[4])/BW_r[0]) + (lat[0] + lat[1]) + (lat[0] + 
 T[0][1] = encode[1] + ((video_size[4])/BW_r[1]) + (lat[1]) + (lat[1])
 T[0][2] = encode[2] + ((video_size[4])/BW_r[2]) + (1e-3)
 T[0][3] = encode[3] + ((video_size[4])/BW_r[3]) + (1e-3)
+T[0][4] = encode[4] + ((video_size[4])/BW_r[4]) + (1e-3)
 
 T[1][0] = splittingstream[0] +  (60*(seg_size[index_of_segment])/BW_r[0]) + (lat[0] + lat[1]) + (lat[0] + lat[1])
 T[1][1] = splittingstream[1] +  (60*(seg_size[index_of_segment])/BW_r[1]) + (lat[1]) + (lat[1])
 T[1][2] = splittingstream[2] +  (60*(seg_size[index_of_segment])/BW_r[2]) + (1e-3) + (1e-3)
 T[1][3] = splittingstream[3] +  (60*(seg_size[index_of_segment])/BW_r[3]) + (1e-3) + (1e-3)
+T[1][4] = splittingstream[4] +  (60*(seg_size[index_of_segment])/BW_r[4]) + (1e-3) + (1e-3)
 
 T[2][0] = training[0] + (60*(seg_size[index_of_segment])/BW_r[0]) + (lat[0] + lat[1]) + (lat[0] + lat[1])
 T[2][1] = training[1] + (60*(seg_size[index_of_segment])/BW_r[1]) + (lat[1]) + (lat[1])
 T[2][2] = training[2] + (60*(seg_size[index_of_segment])/BW_r[2]) + (1e-3) + (1e-3)
 T[2][3] = training[3] + (60*(seg_size[index_of_segment])/BW_r[3]) + (1e-3) + (1e-3)
+T[2][4] = training[4] + (60*(seg_size[index_of_segment])/BW_r[4]) + (1e-3) + (1e-3)
+
+print(T)
 
 fileObject = open("D:\\00Research\\matching\\scheduler\\ADA-PIPE-Frontend-new\\MOG-BC2\\match-model\\steps.yml", 'w').close()
 fileObject = open("D:\\00Research\\matching\\scheduler\\ADA-PIPE-Frontend-new\\MOG-BC2\\match-model\\resources.yml",'w').close()
 	
 #sorted(iterable, *, key=None, reverse=False)
 for i in range(len(microservices)):
-	for j in range(len(resources)):		
+	for j in range(len(resources)):
 		dictlistMicroservices[i][(resources[j],microservices[i])] = numpy.round((T[i][j]),4)
-	sorted_dictlistMicroservices[i]=sorted(dictlistMicroservices[i].items(),key = itemgetter(1))
+	sorted_dictlistMicroservices[i]=sorted(dictlistMicroservices[i].items(),key = itemgetter(1), reverse=False)
 	mpllll=dict(dict(sorted_dictlistMicroservices[i]).keys())
 	listofvalues = list(mpllll.keys())
 	listofkeys=list(mpllll.values())
