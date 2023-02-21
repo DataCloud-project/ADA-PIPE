61e7cc856b6577ce74108086cdf56d0511a68de8
Narges Mehran <39765584+SiNa88@users.noreply.github.com>
2023-02-22 00:06:07 +0100

    Update parseDSL.py

--- ImportFrom-DEF-PIPE/parseDSL.py
+++ ImportFrom-DEF-PIPE/parseDSL.py
@@ -7,7 +7,7 @@ sys.path.append('./parser_constants')
 
 def parseDSL(in_file) -> None:
     count = 0
-    with open('D:\\00Research\\matching\\scheduler\\Demo3\\dsl2json\\3ApplicationLogic.json', 'r') as openfile:
+    with open('3ApplicationLogic.json', 'r') as openfile:
         requirement_settings = json.load(openfile)
     for line in in_file.splitlines():
         words = line.split(' ')
@@ -15,36 +15,36 @@ def parseDSL(in_file) -> None:
         STEPS_NAME =  "temp"
         if len(words) != 1:
             if (PIPELINE_NAME in words[0]):
-                requirement_settings["pipelineName"] = str(words[1])
-                requirement_settings["chunkName"] = words[1]+"UseCase"
-
+                requirement_settings["pipelineName"] = str("TelluPipeline")
+                requirement_settings["chunkName"] = "Tellu"+"UseCase"
             elif (words[0] == STEPS_REQ_MINCPU):
-                requirement_settings["stepsList"][0]["requirement"]["vCPUs"] = ((float(words[1]))/1024.0)
-
+                requirement_settings["stepsList"][count]["requirement"]["vCPUs"] = ((float(words[1]))/1024.0)
             elif (words[0] == STEPS_REQ_MINMEM):
-                requirement_settings["stepsList"][0]["requirement"]["ram"] = words[1]
-
+                requirement_settings["stepsList"][count]["requirement"]["ram"] = words[1]
             elif "data-processing step" in line or "data-source step" in line or "data-sink step" in line:
-                requirement_settings["stepsList"][0]["name"] = STEPS_NAME = words[3].lower().strip()
-                requirement_settings["stepsList"][0]["dockerImage"] = "datacloud2.itec.aau.at/"+STEPS_NAME+":latest"
-                requirement_settings["stepsList"][0]["dockerRegistry"] = "datacloud2.itec.aau.at/"
-                requirement_settings["stepsList"][0]["dockerUsername"] = "mogsport"
-                #requirement_settings["stepsList"][0]["dockerPassword"] = ""
-
-            #elif (words[0] == STEPS_IMPLEMENTATION):
-            #    requirement_settings["stepsList"][0]["dockerImage"] = words[3].lower()
-
-            #elif (words[0] == STEPS_REQ_MAXINS):
-            #    requirement_settings = "???"
-
-            if (count == 0):
-                requirement_settings["stepsList"][0]["provider"] = "Tellu"
-                requirement_settings["stepsList"][0]["resource"] = "Tellu Gateway 0"
-            else:
-                requirement_settings["stepsList"][0]["provider"] = "DataCloud"
-                requirement_settings["stepsList"][0]["resource"] = "DatacloudWP1"
-            with open("D:\\00Research\\matching\\scheduler\\Demo3\\dsl2json\\"+STEPS_NAME+".json", "w") as outfile:
-                json.dump(requirement_settings, outfile)
+                #print(count)
+                if (count == 0):
+                  requirement_settings["stepsList"][count]["provider"] = "Tellu"
+                  requirement_settings["stepsList"][count]["resource"] = "Tellu Gateway 0"
+                  requirement_settings["stepsList"][count]["name"] = STEPS_NAME = words[3].lower().strip()
+                  requirement_settings["stepsList"][count]["dockerImage"] = "datacloud2.itec.aau.at/"+STEPS_NAME+":latest"
+                  requirement_settings["stepsList"][count]["dockerRegistry"] = "datacloud2.itec.aau.at/"
+                  requirement_settings["stepsList"][count]["dockerUsername"] = "telluuser"
+                elif (count == 1):
+                  requirement_settings["stepsList"][count]["provider"] = "DataCloud"
+                  requirement_settings["stepsList"][count]["resource"] = "Datacloud-wp1-test1"
+                  requirement_settings["stepsList"][count]["name"] = STEPS_NAME = words[3].lower().strip()
+                  requirement_settings["stepsList"][count]["dockerImage"] = "datacloud2.itec.aau.at/"+STEPS_NAME+":latest"
+                  requirement_settings["stepsList"][count]["dockerRegistry"] = "datacloud2.itec.aau.at/"
+                  requirement_settings["stepsList"][count]["dockerUsername"] = "telluuser"
+                elif (count == 2):
+                  requirement_settings["stepsList"][count]["provider"] = "DataCloud"
+                  requirement_settings["stepsList"][count]["resource"] = "Datacloud-wp1-test2"
+                  requirement_settings["stepsList"][count]["name"] = STEPS_NAME = words[3].lower().strip()
+                  requirement_settings["stepsList"][count]["dockerImage"] = "datacloud2.itec.aau.at/"+STEPS_NAME+":latest"
+                  requirement_settings["stepsList"][count]["dockerRegistry"] = "datacloud2.itec.aau.at/"
+                  requirement_settings["stepsList"][count]["dockerUsername"] = "telluuser"
+                  #requirement_settings["stepsList"][0]["dockerPassword"] = ""
                 count += 1
 
 
@@ -56,7 +56,7 @@ if __name__ == "__main__":
     output, err = p.communicate()
     dict = json.loads(output.decode())
     user_token_ = dict["access_token"]
-    def_call = ["curl", "-X", "GET", "https://crowdserv.sys.kth.se/api/repo/export/testuser/pipeline",
+    def_call = ["curl", "-X", "GET", "https://crowdserv.sys.kth.se/api/repo/export/testuser/TelluPipeline",
                 "-H", "accept: application/json", "-H", "Authorization: Bearer {}".format(user_token_)]
     p = subprocess.Popen(def_call, stdout=subprocess.PIPE,
                          stderr=subprocess.PIPE)
