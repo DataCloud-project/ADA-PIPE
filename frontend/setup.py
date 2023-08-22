c52b37bcb75473950c57b0665d5d6c758d9dc72c
Narges Mehran <39765584+SiNa88@users.noreply.github.com>
2023-08-22 23:49:16 +0200

    Delete frontend directory

--- frontend/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='json_utils',
-    packages=['templates', 'static', 'test'],
-    include_package_data=True,
-    install_requires=[
-        'flask',
-],)
