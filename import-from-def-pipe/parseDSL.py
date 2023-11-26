8f868a672d49542806e73f90f93f70b35eb053c9
Narges Mehran <39765584+SiNa88@users.noreply.github.com>
2023-11-26 22:01:21 +0100

    Update parseDSL.py

--- import-from-def-pipe/parseDSL.py
+++ import-from-def-pipe/parseDSL.py
@@ -1,11 +1,11 @@
 import subprocess
 import json
 from parser_constants import *
-import sys
-sys.path.append('./parser_constants')
+#import sys
+#sys.path.append('./parser_constants')
 
 
-def parseDSL(in_file) -> None:
+def parseDSLTellu(in_file) -> None:
     count = 0
     with open('3ApplicationLogic.json', 'r') as openfile:
         requirement_settings = json.load(openfile)
@@ -49,18 +49,58 @@ def parseDSL(in_file) -> None:
                 json.dump(requirement_settings, outfile)
 
 if __name__ == "__main__":
-    token_call = ["curl", "--location", "--request", "POST", "https://datacloud-auth.euprojects.net/auth/realms/user-authentication/protocol/openid-connect/token", "--header", "Content-Type: application/x-www-form-urlencoded",
-                  "--data-urlencode", "username=?????", "--data-urlencode", "password=??????", "--data-urlencode", "realm=user-authentication", "--data-urlencode", "client_id=def_frontend", "--data-urlencode", "grant_type=password"]
+    with open('passwords.json', 'r') as openfile:
+        passes = json.load(openfile)
+    user = "testuser" # "moguser"; "tluser"; "jotuser"; "boschuser"; "catuser"
+    token_call = ["curl", "--location", "--request", "POST", "https://datacloud-auth.euprojects.net/auth/realms/user-authentication/protocol/openid-connect/token", "--header", "Content-Type: application/x-www-form-urlencoded", "--data-urlencode", "username="+user, "--data-urlencode", "password="+passes[user],
+                   "--data-urlencode", "realm=user-authentication", "--data-urlencode", "client_id=def_frontend", "--data-urlencode", "grant_type=password"]
     p = subprocess.Popen(token_call, stdout=subprocess.PIPE,
                          stderr=subprocess.PIPE)
     output, err = p.communicate()
     dict = json.loads(output.decode())
     user_token_ = dict["access_token"]
-    def_call = ["curl", "-X", "GET", "https://crowdserv.sys.kth.se/api/repo/export/testuser/TelluPipeline",
+    #                                                                                       
+    def_call = ["curl", "-X", "GET", "https://crowdserv.sys.kth.se/api/repo/export/testuser/d0bc11ef-2416-41fa-9357-ff0e2fbbdec4",
                 "-H", "accept: application/json", "-H", "Authorization: Bearer {}".format(user_token_)]
     p = subprocess.Popen(def_call, stdout=subprocess.PIPE,
                          stderr=subprocess.PIPE)
     output_of_def, err = p.communicate()
     dict = json.loads(output_of_def.decode())
     print(dict["data"])
-    parseDSL(dict["data"])
+    parseDSLTellu(dict["data"])
+
+    def_call = ["curl", "-X", "GET", "https://crowdserv.sys.kth.se/api/repo/export/testuser/6a77d0fa-ae70-4372-9c44-08bb6fb1d7f6",
+                "-H", "accept: application/json", "-H", "Authorization: Bearer {}".format(user_token_)]
+    p = subprocess.Popen(def_call, stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE)
+    output_of_def, err = p.communicate()
+    dict = json.loads(output_of_def.decode())
+    print(dict["data"])
+    #parseDSLBosch(dict["data"])
+
+    def_call = ["curl", "-X", "GET", "https://crowdserv.sys.kth.se/api/repo/export/testuser/e1536753-a88e-4489-85e4-84d555500e58",
+                "-H", "accept: application/json", "-H", "Authorization: Bearer {}".format(user_token_)]
+    p = subprocess.Popen(def_call, stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE)
+    output_of_def, err = p.communicate()
+    dict = json.loads(output_of_def.decode())
+    print(dict["data"])
+    #parseDSLMog(dict["data"])
+
+    def_call = ["curl", "-X", "GET", "https://crowdserv.sys.kth.se/api/repo/export/testuser/468532e8-580e-4370-b246-352433cb76a7",
+                "-H", "accept: application/json", "-H", "Authorization: Bearer {}".format(user_token_)]
+    p = subprocess.Popen(def_call, stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE)
+    output_of_def, err = p.communicate()
+    dict = json.loads(output_of_def.decode())
+    print(dict["data"])
+    #parseDSLJot(dict["data"])
+
+    def_call = ["curl", "-X", "GET", "https://crowdserv.sys.kth.se/api/repo/export/testuser/a6d3bea4-dc77-4a56-a086-4c6eab2ae4c4",
+                "-H", "accept: application/json", "-H", "Authorization: Bearer {}".format(user_token_)]
+    p = subprocess.Popen(def_call, stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE)
+    output_of_def, err = p.communicate()
+    dict = json.loads(output_of_def.decode())
+    print(dict["data"])
+    #parseDSLCer(dict["data"])
